4295e78f436f7bf346c759302fc61897cc9e8ffd
tinaaaaa42 <805210884@qq.com>
2022-11-22 23:32:55 +0800

    Finish the optional part of BSTMap

--- lab9/lab9/BSTMap.java
+++ lab9/lab9/BSTMap.java
@@ -1,5 +1,10 @@
 package lab9;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
 
@@ -112,7 +117,12 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
     /* Returns a Set view of the keys contained in this map. */
     @Override
     public Set<K> keySet() {
-        throw new UnsupportedOperationException();
+        Set<K> set = new HashSet<>();
+        Iterator<K> iterator = new BSTIterator<>();
+        while (iterator.hasNext()) {
+            set.add(iterator.next());
+        }
+        return set;
     }
 
     /** Removes KEY from the tree if present
@@ -121,7 +131,59 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
      */
     @Override
     public V remove(K key) {
-        throw new UnsupportedOperationException();
+        V temp = get(key);
+        if (temp == null) {
+            return null;
+        }
+        Node node = locate(key, root);
+        deleteTop(node);
+        size -= 1;
+        return temp;
+    }
+
+    /** returns the Node with the given key under p
+     *  precondition: key exists under p */
+    private Node locate(K key, Node node) {
+        if (node.key.equals(key)) {
+            return node;
+        } else if (node.key.compareTo(key) > 0) {
+            return locate(key, node.left);
+        } else {
+            return locate(key, node.right);
+        }
+    }
+
+    /** delete the top under p (going down in left if existing) */
+    public void deleteTop(Node p) {
+        if (p.left != null) {
+            Node downNode = p.left;
+            Node parent = p;
+            if (downNode.right == null) {
+                p.left = downNode.left;
+                p = downNode;
+            }
+            while (downNode.right != null) {
+                parent = downNode;
+                downNode = downNode.right;
+            }
+            parent.right = downNode.left;
+            p = downNode;
+        } else if (p.right != null) {
+            Node downNode = p.right;
+            Node parent = p;
+            if (downNode.left == null) {
+                p.right = downNode.right;
+                p = downNode;
+            }
+            while (downNode.left != null) {
+                parent = downNode;
+                downNode = downNode.left;
+            }
+            parent.left = downNode.right;
+            p = downNode;
+        } else {
+            p = null;
+        }
     }
 
     /** Removes the key-value entry for the specified key only if it is
@@ -130,11 +192,75 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
      **/
     @Override
     public V remove(K key, V value) {
-        throw new UnsupportedOperationException();
+        V temp = get(key);
+        if (temp == null) {
+            return null;
+        }
+        if (temp.equals(value)) {
+            Node node = locate(key, root);
+            deleteTop(node);
+            size -= 1;
+            return temp;
+        }
+        return null;
     }
 
     @Override
     public Iterator<K> iterator() {
-        throw new UnsupportedOperationException();
+        return new BSTIterator<K>();
     }
+
+    private class BSTIterator<K> implements Iterator<K> {
+        private ArrayList<Node> nodeList;
+        private Node node;
+        private int cnt;
+        public BSTIterator() {
+            node = root;
+            nodeList = new ArrayList<>();
+            nodeList.add(root);
+            cnt = 0;
+        }
+        @Override
+        public boolean hasNext() {
+            return cnt < size;
+        }
+
+        @Override
+        public K next() {
+            for (Node n: nodeList) {
+                K temp = (K) n.key;
+                if (!isOccupied(n.left)) {
+                    nodeList.add(n.left);
+                }
+                if (!isOccupied(n.right)) {
+                    nodeList.add(n.right);
+                }
+                nodeList.remove(n);
+                cnt += 1;
+                return temp;
+            }
+            return null;
+        }
+
+        private boolean isOccupied(Node n) {
+            return n == null || nodeList.contains(n);
+        }
+    }
+
+    @Test
+    public void testRemove() {
+        BSTMap<String, Integer> b = new BSTMap<String, Integer>();
+        b.put("a", 1);
+        assertTrue(((Integer) b.remove("a")).equals(1));
+        assertEquals(0, b.size());
+
+        b.put("d", 2);
+        b.put("b", 3);
+        b.put("c", 4);
+        assertTrue(((Integer) b.remove("b")).equals(3));
+        assertEquals(2, b.size());
+
+        assertTrue(b.remove("d", 1) == null);
+    }
+
 }
